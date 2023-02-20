601c40a7f243bbddfe6c3f745e12576cb46e3121
운동 <112850163+Undong00@users.noreply.github.com>
2023-02-20 11:30:44 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.06 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120583. 중복된 숫자 개수/중복된 숫자 개수.js
@@ -0,0 +1,10 @@
+function solution(array, n) {
+    let a = 0;
+    for(let i = 0; i<array.length; i++){
+        if(array[i] == n){
+            a +=1 ;
+            
+        }
+    }
+    return a;
+}
