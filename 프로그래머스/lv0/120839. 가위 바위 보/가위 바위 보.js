d5d932d87de9baacfe3e2720907845218fc2c8cd
운동 <112850163+Undong00@users.noreply.github.com>
2023-02-21 09:33:55 +0900

    [level 0] Title: 가위 바위 보, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.js
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.js
@@ -1,16 +1,3 @@
 function solution(rsp) {
-    let rspg = ''
-   for(let i =0; i<rsp.length; i++){
-       let ca = rsp.charAt(i);
-       if(ca === "2"){
-           rspg += "0"
-       }
-       else if(ca === "0"){
-           rspg += "5"
-       }
-       else if(ca === "5"){
-           rspg += "2"
-       }  
-   }
-    return rspg
+    return rsp.split("").reduce((a,b,c)=>2,0,5)?0:5:2;
 }
