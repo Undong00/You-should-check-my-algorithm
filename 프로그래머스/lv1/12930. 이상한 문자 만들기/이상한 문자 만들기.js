d25a549188d791c1d93e7b91c111219e4e1c10d3
운동 <112850163+Undong00@users.noreply.github.com>
2023-04-07 17:11:35 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 3.10 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -0,0 +1,23 @@
+
+function solution(s) {
+    let Box = s.split(" ")  // ["try", "hello", "world"]
+    let resultBox = []
+    // let resultBox = ``
+    for(let i=0; i < Box.length; i++){
+        let innerBox =""
+        for(let j=0; j < Box[i].length; j++){
+            if(j%2 === 0){
+                console.log(Box[i][j].toUpperCase())
+                innerBox += Box[i][j].toUpperCase() // => T Y
+                // innerBox += Box[i][j].toUpperCase() // => T Y
+            } else {
+                innerBox += Box[i][j].toLowerCase()  // r (만약 대문자면 소문자로)
+            }
+        } // "TrY"
+        resultBox.push(innerBox)  // => ["TrY", "HeLlO", "WoRlD"]
+    }
+    return resultBox.join(" ")
+    // return resultBox.reduce((prev, curv)=> {
+    //     `${prev}, ${curv}`
+    // })
+}
