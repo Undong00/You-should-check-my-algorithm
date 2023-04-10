1cbb263c124946382ffd592852ff2a041b36ab2f
운동 <112850163+Undong00@users.noreply.github.com>
2023-04-10 17:09:00 +0900

    [level 1] Title: 예산, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12982. 예산/예산.js
@@ -0,0 +1,13 @@
+function solution(d, budget) {
+  let box = d.sort((a, b) => a - b); // 오름차순으로 정렬
+  let count = 0;
+  for (let i = 0; i < box.length; i++) {
+    if (budget >= box[i]) {
+      budget -= box[i];
+      count++;
+    } else {
+      break;
+    }
+  }
+  return count;
+}
