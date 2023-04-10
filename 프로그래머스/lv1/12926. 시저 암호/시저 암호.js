96da6c2761ab0513c36869ec8b5a0bc48e857428
운동 <112850163+Undong00@users.noreply.github.com>
2023-04-10 11:43:57 +0900

    [level 1] Title: 시저 암호, Time: 2.79 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.js
@@ -0,0 +1,18 @@
+function solution(s, n) {
+    // let dict = /[a-z]/g        // "abcdefghijklmnopqrstuvwxyz"
+    let dict = "abcdefghijklmnopqrstuvwxyz"
+    let bigDict = dict.toUpperCase()     // "ABCDE....................Z"
+    console.log(bigDict)
+    let sBox = s.split("")  
+    return sBox.map((ele,idx)=> {
+        if(ele === " "){
+            return " " // " "
+        } else if(ele > "Z" ){ 
+            return dict[(dict.indexOf(ele) + n) % 26] 
+        } else { 
+            return bigDict[(bigDict.indexOf(ele) + n) % 26] 
+        }
+    }).join("")
+
+}
+
